dc97b8f1c8204edafd448a29ab7d0e972b4d0982
wanglizhong0118 <wanglizhong0118@gmail.com>
2018-04-05 22:12:29 +0200

    Feat: 180405 fix

--- Kattis/src/solved/Kornislav.java
+++ Kattis/src/solved/Kornislav.java
@@ -1,29 +1,32 @@
 package solved;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
- * https://open.kattis.com/problems/arithmetic
+ * https://open.kattis.com/problems/drmmessages
  * 
  * @author allwi
  *
  */
-public class Arithmetic {
+public class Kornislav {
 
     public static void main(String[] args) throws NumberFormatException, IOException {
 
         final BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         final BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        final String in = r.readLine();
-        BigInteger bi = new BigInteger(in, 8);
-        String hex = bi.toString(16);
-        w.write(String.valueOf(hex).toUpperCase());
+        final String[] sts = r.readLine().split(" ");
+        List<Integer> ls = new ArrayList<>();
+        for (int i = 0; i < sts.length; i++) {
+            ls.add(Integer.parseInt(sts[i]));
+        }
+        Collections.sort(ls);
+        w.write(String.valueOf(ls.get(0) * ls.get(2)));
         w.flush();
         w.close();
         r.close();
